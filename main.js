d5c62b538eeb4ee1dec63cbb8c04a3056c7d65b0
soubhik-111 <soubhikbanerjee04@gmail.com>
2023-04-16 13:16:33 +0530

    error handling

--- main.js
+++ main.js
@@ -5,28 +5,45 @@ const options = {
         'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
     }
 };
-
+let cityname = "Dhanbad";
 const getWeather = (city) => {
-    cityName.innerHTML = city
+    // document.getElementById('cityname').innerHTML = city
 
     fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`, options)
         .then(response => response.json())
         .then(response => {
-
-
             console.log(response)
-            cloud_pct.innerHTML = response.cloud_pct
-            temp.innerHTML = response.temp
-            temp1.innerHTML = response.temp
-            feels_like.innerHTML = response.feels_like
-            humidity.innerHTML = response.humidity
-            humidity1.innerHTML = response.humidity
-            min_temp.innerHTML = response.min_temp
-            max_temp.innerHTML = response.max_temp
-            wind_speed.innerHTML = response.wind_speed
-            wind_speed1.innerHTML = response.wind_speed
-            sunrise.innerHTML = response.sunrise
-            sunset.innerHTML = response.sunset
+            if (response.error) {
+                msg.innerText=response.error;
+                cloud_pct.innerHTML = '-';
+                temp.innerHTML = '-';
+                temp1.innerHTML = '-';
+                feels_like.innerHTML = '-';
+                humidity.innerHTML = '-';
+                humidity1.innerHTML = '-';
+                min_temp.innerHTML = '-';
+                max_temp.innerHTML = '-';
+                wind_speed.innerHTML = '-';
+                wind_speed1.innerHTML = '-';
+                sunrise.innerHTML = '-';
+                sunset.innerHTML = '-';
+            }
+            else {
+                cityname=city;
+                msg.innerText=`Weather for ${city}`
+                cloud_pct.innerHTML = response.cloud_pct
+                temp.innerHTML = response.temp
+                temp1.innerHTML = response.temp
+                feels_like.innerHTML = response.feels_like
+                humidity.innerHTML = response.humidity
+                humidity1.innerHTML = response.humidity
+                min_temp.innerHTML = response.min_temp
+                max_temp.innerHTML = response.max_temp
+                wind_speed.innerHTML = response.wind_speed
+                wind_speed1.innerHTML = response.wind_speed
+                sunrise.innerHTML = response.sunrise
+                sunset.innerHTML = response.sunset
+            }
         })
         .catch(err => console.error(err));
 }
@@ -39,9 +56,9 @@ let refresh_tag = document.querySelectorAll(".refresh")
 
 for (let index = 0; index < refresh_tag.length; index++) {
     refresh_tag[index].addEventListener("click", () => {
-        location.reload()
+        getWeather(cityname)
     })
-
 }
 
-getWeather("Dhanbad")
+
+getWeather(cityname)
